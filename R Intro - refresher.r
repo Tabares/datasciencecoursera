c6482b7fc98a02654639b215344217ad597f92fe
adalberto-g <adalberto.gonzalezayala@gmail.com>
2014-05-09 11:16:51 -0500

    comments added to all sections

--- R Intro - refresher.r
+++ R Intro - refresher.r
@@ -12,7 +12,7 @@ setwd("/Adl Box/My Box Files/mooc CompDataA.R/Week 1")  #or cmd+d
 --------------------------------------
     
 ##R 1.2 - Calculations and Variables
-
+#Get acquainted with performing basic computations in R, creating R objects (sometimes called "variables"), and seeing and running earlier commands that were run in the console.
 sqrt(16)
 factorial(4)
 4*3*2*1
@@ -26,7 +26,7 @@ my.height
 --------------------------------------
     
 ##R 1.3 - Create and Work With Vectors
-
+##Learn how to create a vector in R using the c() function and the colon-notation (e.g. 1:5), find the length of a vector using the length() function, and subset vectors with bracket-notation and the head() and tail() functions.
 c(2,3,5,7,11,13,17,19,23,101)  
 v <- c(2,3,5,7,11,13,17,19,23,101)
 v[3]
@@ -43,7 +43,8 @@ tail(v,3)
 --------------------------------------
   
 ##R 1.4 - Character and Boolean Vectors
-
+##So far we've seen objects that use numbers, but R is also built to handle more types of objects. Here we explore vectors that contain strings or Boolean values (TRUE / FALSE).
+  
 v<-c("Gangnam style","charlie biut my finger", "evolutuion of dance","rickroll","lady Gaga")
 length(v)
 
@@ -59,6 +60,7 @@ v[under.10]
 --------------------------------------
 
 ##R 1.5 - Vector Arithmetic
+##This video demystifies the different ways R performs vector arithmetic (e.g. addition and multiplication), covering topics including element-wise arithmetic, vector recycling, and how some functions are automatically applied across the elements in a vector. The seq() function is also introduced.
 
 v <- c(10,20,30,40,50,60)
 v+1
@@ -84,7 +86,8 @@ sqrt(v)
 ------------------------------------------
 
 ##R 1.6 - Building and Subsetting Matrices
-
+##Learn how to create a matrix with dimensions of your choosing, how recycle a vector when creating a matrix, and how to query specific characteristics of the vector using functions such as dim(), head(), and tail(). Details and potential pitfalls for subsetting a matrix using the bracket-notation are also covered.
+  
 v <- 1:12   # create vector first and the matrix
   
 matrix(v, nrow=3, ncol=4)
@@ -167,7 +170,9 @@ airquality[good,][1:6,] #head(airquality[good,])
 --------------------------------------------------------
 
 ##R 1.7 - Section 1 Review and Help Files
-
+##A quick review of the topics covered in the Section 1 videos: vectors, matrices, subsetting, and a few standard functions. Also learn how to access help files in R.
+  
+  
 c(2,3,5,7,1:5)
 25:30
 v <- 25:60
@@ -186,7 +191,8 @@ m[1:2,3:5]
 --------------------------------------------------------
   
 ##R 2.1 Loading Data and Working With Data Frames 
-
+##Get a refresher on navigating directories on your computer in R, and learn to load a CSV (comma-separated values) data set in the form of a "data frame" using the read.csv() function, which is a special type of data matrix. This video also introduces factor variables and explores the data in a data frame using the dim(), head(), length(), names(), and subset() functions.
+  
 getwd()
 list.files()
 setwd("/Adl Box/My Box Files/mooc CompDataA.R/Week 1")
@@ -215,7 +221,8 @@ as.character(data$Verified) #convert the factor to a string
 -----------------------------------------------
   
 ##R 2.2 - Loading Data, Object Summaries, and Dates
-
+##Learn how to load data in the form of a tab-delimited text file using the read.delim(), how to get a high-level overview of an R object using the str() and summary() functions, and get a crash-course into working with dates in R with an example highlighting why this skill is so useful.
+  
 setwd("/Adl Box/My Box Files/mooc CompDataA.R/Week 1")
 stocks <- read.delim("stocks.txt") #tab delimited
 head(stocks,3)
@@ -255,7 +262,8 @@ GOOG
 ---------------------------------------------
 
 ##R 2.3 - if() Statements, Logical Operators, and the which() Function
-
+##if-else statements are a key component to any programming language. This video introduces how to effectively use these statements in R and also clarifies some nuances of logical operators in R. Two related functions are also introduced: ifelse() as a shortcut that can be used to create faster and more readable code, and the which() function that retrieves the positions in a Boolean vector that are TRUE.
+  
 setwd("/Adl Box/My Box Files/mooc CompDataA.R/Week 1")
 stocks <- read.delim("stocks500T.txt") #stocks500Test.txt
 names(stocks)
@@ -298,10 +306,25 @@ big.change<- abs( fluctuation) > 0.25
 head( big.change) #boolean vector of true and false
 which( big.change) #return trues
 
+
+#other valid if structures
+if(x>3){
+  y<- 10
+}else{
+  y<- 0
+}
+or
+y <- if(x>3){
+  10
+}else{
+  0
+}
+
 --------------------------------------
 
 ##R 2.4 - for() Loops and Handling Missing Observations
-
+##This video discusses for() loops, which are a structure that can be used to execute a set of code repeatedly. Also covered in this video are the min(), max(), and append() functions, as well as how to identify and omit missing values.
+  
 setwd("/Adl Box/My Box Files/mooc CompDataA.R/Week 1")
 stocks <- read.delim("stocks500T.txt") #stocks500Test.txt
 names( stocks)
@@ -328,8 +351,39 @@ head(highs)
 sum(is.na(stocks$low))
 sum(is.na(stocks$high))
 
+#other cicles
+#While
+count <- 0
+while(count <10){
+  print (count)
+  count <- count +1
+}
+
+#repeat
+x0 <- 1
+to1 <- 1e-8
+repeat{
+  x1 <- computeEstimate()
+  if (abs(x1-x0)< to1){
+    break
+  }else{
+    x0 <- x1
+  }
+}
+#next
+for ( i in 1:100){
+  if(i <= 20){
+    #skip first 20 iterations
+    next
+  }
+  #do something here
+  print ( i)
+}
+
 --------------------------------------------------
 ##R 2.5 - Lists
+##Learn how to create and use lists in R, which are dynamic, flexible R objects that can hold and organize other R objects.
+  
 x<- list(1,"a", TRUE, 1+4i)
 x
 x[["newval"]] <- "123"
@@ -366,7 +420,8 @@ stock.summary$TSLA
 ------------------------------------------------
   
 ##R 3.1 - Managing the Workspace and Variable Casting
-View all the objects in the workspace using ls(), remove objects using rm(), cast a variable as a different type using an as.[type]() function, and use lists or data frames to organize data or results.
+##View all the objects in the workspace using ls(), remove objects using rm(), cast a variable as a different type using an as.[type]() function, and use lists or data frames to organize data or results.
+  
 
 a <- 1
 b <- 2
@@ -403,7 +458,7 @@ dr$mean
 ------------------------------------------------
   
 ##R 3.2 - The apply() Family of Functions
-  When data are organized in a matrix or data frame, the apply() function can be used to calculate summaries (or apply a more complex function) across either the rows or columns of the data object. Or if summaries for each group (level) of one or more variables are desired, use the tapply() or by() function.  
+##When data are organized in a matrix or data frame, the apply() function can be used to calculate summaries (or apply a more complex function) across either the rows or columns of the data object. Or if summaries for each group (level) of one or more variables are desired, use the tapply() or by() function.  
 
 rm(list=ls())
 rev <- read.csv("revenue.csv", header=FALSE)
@@ -468,8 +523,7 @@ c(x)
 ------------------------------------------------
 
 ##R 3.3 - Access or Create Columns in Data Frames, or Simplify a Data Frame using aggregate
-  
-The with() function makes it easy to access many variables (columns) in a data frame for one-off calculations, the within() function can be used to create new columns that are merged with the original data frame, and aggregate() is useful for aggregating variables in a data frame across groupings based on one or more variables.
+##The with() function makes it easy to access many variables (columns) in a data frame for one-off calculations, the within() function can be used to create new columns that are merged with the original data frame, and aggregate() is useful for aggregating variables in a data frame across groupings based on one or more variables.
   
 setwd("/Adl Box/My Box Files/mooc CompDataA.R/week 1")
 mariokart <- read.delim("mariokart.txt")
@@ -494,9 +548,9 @@ aggregate( totalPr ~ wheels + cond, mk, mean )
 ------------------------------------------------
 
 ##R 4.1 - Basic Structure of a Function
-
 ##introduces the basic structure of a function, covering the declaration of the function, using an argument, and returning a result.
-set.seed(100) #ensure dataset is stable by using seed
+
+  set.seed(100) #ensure dataset is stable by using seed
 d <- rpois(25,8) #poisson dist 8 deg free
 d   #if i run this code ill get the same dataset
 
@@ -532,8 +586,8 @@ GetCI(d,99)  #it would be nice to report the error from function
   
 ------------------------------------------------
   
-  #R 4.3 - Add a Warning or Stop the Function Execution
-  #The most helpful functions return clear warnings and errors when something is wrong. This video introduces the warning() and stop() functions, which can be used within a function to report a warning to the user or stop the function's execution.
+#R 4.3 - Add a Warning or Stop the Function Execution
+#The most helpful functions return clear warnings and errors when something is wrong. This video introduces the warning() and stop() functions, which can be used within a function to report a warning to the user or stop the function's execution.
 
 set.seed(100) #ensure dataset is stable by using seed
 d <- rpois(25,8) #poisson dist 8 deg free
@@ -558,8 +612,9 @@ GetCI <- function(x, level=0.95){
 GetCI(d,0.05)  #it would be nice to report the error from function
   
 ------------------------------------------------
-  #R 4.4 - Passing Additional Arguments Using an Ellipsis
-  #Sometimes it is useful to be able to pass any extra arguments to another function. For example, if a new plotting function is created that makes use of the function called plot(), it would be useful to be able to be able to specify additional details to plot() automatically. This is possible using an ellipsis, "...".
+#R 4.4 - Passing Additional Arguments Using an Ellipsis
+#Sometimes it is useful to be able to pass any extra arguments to another function. For example, if a new plotting function is created that makes use of the function called plot(), it would be useful to be able to be able to specify additional details to plot() automatically. This is possible using an ellipsis, "...".
+
   rm(list=ls())
   setwd("/Adl Box/My Box Files/mooc CompDataA.R/week 1")
   revenue <- read.csv("revenue.csv", header=FALSE)
@@ -581,9 +636,10 @@ RevSummary(revenue, na.rm=TRUE)
 which(is.na(revenue), arr.ind=TRUE)
 
 ------------------------------------------------
-  #R 4.5 - Make a Returned Result Invisible and Build Recursive Function
-  #Use the invisible() function in place of return() in a function to prevent the output from printing to the user's screen but still allow the result to be stored into an R object. Additionally, when building a recursive function, use the Recall() function.
-annoying <- function(x){
+#R 4.5 - Make a Returned Result Invisible and Build Recursive Function
+#Use the invisible() function in place of return() in a function to prevent the output from printing to the user's screen but still allow the result to be stored into an R object. Additionally, when building a recursive function, use the Recall() function.
+
+  annoying <- function(x){
   #return(rep(x,1000))
   invisible(rep(x,1000)) 
 }
@@ -605,8 +661,8 @@ logMe(3.392)
 logMe(exp(3.392))
 
 ------------------------------------------------
-  #R 4.6 - Custom Functions With apply()
-  #In the last section of videos, the apply() function was introduced. A prebuilt function is often used in this function, but it is also common to write a new function within apply() or to apply a custom function.
+#R 4.6 - Custom Functions With apply()
+#In the last section of videos, the apply() function was introduced. A prebuilt function is often used in this function, but it is also common to write a new function within apply() or to apply a custom function.
   
 x <- matrix(rpois(1e4,8), 1e3) 
 apply(x,2,mean)
