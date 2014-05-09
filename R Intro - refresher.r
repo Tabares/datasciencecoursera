5d7486bd0e4dadf2671cccb33869dc2b1882e6bf
adalberto-g <adalberto.gonzalezayala@gmail.com>
2014-05-09 08:48:16 -0500

    4.6 custom funtions with apply

--- R Intro - refresher.r
+++ R Intro - refresher.r
@@ -1,5 +1,4 @@
 #Intro to R
-  
 ##R 1.1 - Initial Setup and Navigation
 ###Strategies that will make beginning R users more efficient: writing code using a "script" and navigating through directories within R
 
@@ -112,6 +111,58 @@ m[1:2,4]
 m[1:2,3:4]
 m[3]
 m[5]
+
+
+x <- list(foo = 1:4, bar = 0.6, baz="hello")
+x[c(1,3)]
+
+x <- list(foo = 1:4, bar = 0.6, baz="hello")
+name <- "foo"
+x[[name]]  #ok
+x$name     #not OK
+x$foo      #ok
+
+x <- list(a = list(10,12,14), b= c(3.14,2.81))
+x
+x[[c(1,3)]] #equal
+x[[1]][[3]] #equal
+x[[c(2,1)]]
+
+
+
+x <- list(aardvark =1:5)
+x$a
+x[["a"]]
+x[["a", exact=FALSE]]  ###partial matching
+
+x<- matrix(1:6, 2,3)
+x[1,2]
+x[1,2, drop=FALSE]  #drops or keeps the dimension
+x[1,, drop=TRUE]
+x[1,, drop=FALSE]
+
+  ###removing missing values
+x <- c(1,2,NA,4, NA,5)
+bad <- is.na(x)
+bad
+mean(x[!bad])
+
+mean(x, na.rm=TRUE)
+mean(x[!bad])
+
+   ##removing NA values
+x <- c(1,2,NA,4, NA,5)
+y <- c("a","b",NA,"d", NA,"f")
+good <- complete.cases(x,y)
+x[good]
+y[good]
+
+setwd("/Adl Box/My Box Files/mooc CompDataA.R/Week 1")
+airquality <- read.csv("01 hw1_data.csv")
+airquality[1:6,] #head(airquality)
+good<- complete.cases(airquality)
+airquality[good,][1:6,] #head(airquality[good,])
+
 
 --------------------------------------------------------
 
@@ -279,6 +330,10 @@ sum(is.na(stocks$high))
 
 --------------------------------------------------
 ##R 2.5 - Lists
+x<- list(1,"a", TRUE, 1+4i)
+x
+x[["newval"]] <- "123"
+
 
 x <- list()
 x
@@ -299,21 +354,311 @@ names(stocks)
 the.tickers <- unique(stocks$ticker)
 
 stock.summary <- list()
-#for.GOOG <- stocks$ticker == "GOOG"
-#summary(stocks[for.GOOG,])
+#these <- stocks$ticker == "GOOG"
+#summary(stocks[these,])
 for( ticker in the.tickers){
-  these <- stocks$ticker == ticker
-  stock.summary[[ticker]] <- summary(stocks[these,]) 
+  these <- stocks$ticker == ticker  #returns a binary file for the coincidences of the ticker
+  stock.summary[[ticker]] <- summary(stocks[these,])  #return the columns from the previous ticker 
 }
 
-stock.summary$GOOG
+stock.summary$TSLA
 
 ------------------------------------------------
   
-R 3.1 - Managing the Workspace and Variable Casting
+##R 3.1 - Managing the Workspace and Variable Casting
+View all the objects in the workspace using ls(), remove objects using rm(), cast a variable as a different type using an as.[type]() function, and use lists or data frames to organize data or results.
+
+a <- 1
+b <- 2
+x <- 1:10
+y <- "ok"
+z <- TRUE
+ls() #look at the objects existing in your current workspace
+rm(c) #remove specific object
+rm( list=ls()) #remove all objects
+
+x
+as.numeric(x)
+as.character(x)
+c<-as.factor(x)
+as.matrix
+as.data.frame
+as.list
+
+NOTE: if you have a factor variable that also looks numeric,
+always convert it to a character beffore converting it to numerical value.
+
+results <- list()
+results$gp1 <- c(40, 22, 16)
+results$gp2 <- c(25, 15, 9)
+results$gp3 <- c(32, 35, 12)
+results$gp4 <- c(28, 19, 15)
+results
+
+ur <-  unlist(results)
+mr <- matrix(ur, 4,3, byrow = TRUE)
+dr <- as.data.frame(mr)
+colnames(dr) <- c("n", "mean", "sd")  #assign names to the individual rows and columns of a data frame,
+dr$mean
+------------------------------------------------
   
+##R 3.2 - The apply() Family of Functions
+  When data are organized in a matrix or data frame, the apply() function can be used to calculate summaries (or apply a more complex function) across either the rows or columns of the data object. Or if summaries for each group (level) of one or more variables are desired, use the tapply() or by() function.  
+
+rm(list=ls())
+rev <- read.csv("revenue.csv", header=FALSE)
+#row  - day mar1 to mar 31
+#column - business
+head( rev, 3)
+
+ # i could take the sum for each column using a forloop
+ # and store that result in a vector but this would actually 
+ # be inneficient
+
+
+ # option a
+total.rev <- c()
+for (i in 1:10) {
+  total.rev <-c( total.rev , sum(rev[,i]))
+} 
+total.rev
+
+ # option b  --APPLY--
+apply (rev, 2, sum) #sum for the month for each business
+#apply ( is.the.data.matrix.itself, dimmension.1=row,2colum , function to be applied) sintaxis
+apply  (rev,1, sum) #summ for each day of the month
+
+ #we can ignore the missing observations
+apply  (rev,1, sum, na.rm=TRUE) 
+
+colSums(rev)
+rowSums(rev)
+
+
+ # second function tapply
+mk <- read.delim("mariokart.txt")
+head(mk,4)
+ #focus on : wheels, cond, totalPr
+summary(mk)
+
+ examine the sum of all of the total prices of auction 
+ that were new and used separately 
+ by providing the vector for the totalPrice as the first argument,
+ the vector for the condition as the second and then the sum funciton
+
+# tapply(data, group, function) sintaxis
+tapply(mk$totalPr, mk$cond, mean)
+tapply(mk$totalPr, mk$wheels, mean)
+
+condider the influnce of both price and number of wheels
+what will this do to the estimated cost of a new game vs a used one
+and also the cost of an extra wheel
+
+we can look at a table of those combinations
+#mk[,c("cond","wheels")]
+tapply(mk$totalPr,mk[ ,c("cond","wheels")], mean)
+tapply(mk$totalPr,mk[ ,c("cond","wheels")], length) #one col has few observations
+ 
+ # the BY function a variant of tapply, 
+ # by default it returns a vector of the results
+
+x<- by(mk$totalPr,mk[ ,c("cond","wheels")], length)
+x[5]
+c(x)
+------------------------------------------------
+
+##R 3.3 - Access or Create Columns in Data Frames, or Simplify a Data Frame using aggregate
+  
+The with() function makes it easy to access many variables (columns) in a data frame for one-off calculations, the within() function can be used to create new columns that are merged with the original data frame, and aggregate() is useful for aggregating variables in a data frame across groupings based on one or more variables.
+  
+setwd("/Adl Box/My Box Files/mooc CompDataA.R/week 1")
+mariokart <- read.delim("mariokart.txt")
+
+head(mariokart)
+closePr <- with(mariokart, totalPr- shipPr)
+head(closePr, 20)
+
+mk <- within(mariokart,{
+  closePr <- totalPr - shipPr
+})
+head(mk)
+
+aggregate(. ~ wheels + cond, mk, mean)
+aggregate( . ~ )
+. ~  keep all the variables for the output
+wheels + cond these separated by a comma indicated wich var to aggregate across
+mk dataframe
+mean is how to aggregate the variables
+aggregate( totalPr ~ wheels + cond, mk, mean )
 
 ------------------------------------------------
+
+##R 4.1 - Basic Structure of a Function
+
+##introduces the basic structure of a function, covering the declaration of the function, using an argument, and returning a result.
+set.seed(100) #ensure dataset is stable by using seed
+d <- rpois(25,8) #poisson dist 8 deg free
+d   #if i run this code ill get the same dataset
+
+  GetMeanAndSE <- function(x){
+    m <- mean(x)
+    n<- length(x)
+    SE <- sd(x)/sqrt(n)
+    return (c(m,SE))
+  }
+
+GetMeanAndSE(d)
+  
+    
+------------------------------------------------
+  
+  #R 4.2 - Returning a List and Providing Default Arguments
+  Become more proficient in writing functions by learning the standard way to return more complex results from functions using a list, and learn how to specify a default value for an argument
+  
+set.seed(100) #ensure dataset is stable by using seed
+d <- rpois(25,8) #poisson dist 8 deg free
+d   #if i run this code ill get the same dataset
+
+GetCI <- function(x, level=0.95){
+  m <- mean(x)
+  n<- length(x)
+  SE <- sd(x)/sqrt(n)
+  upper <- 1-(1-level)/2
+  ci <- m + c(-1,1)*qt(upper, n-1)*SE
+  return (list(mean=m,se=SE,ci=ci))
+}
+
+GetCI(d,99)  #it would be nice to report the error from function
+  
+------------------------------------------------
+  
+  #R 4.3 - Add a Warning or Stop the Function Execution
+  #The most helpful functions return clear warnings and errors when something is wrong. This video introduces the warning() and stop() functions, which can be used within a function to report a warning to the user or stop the function's execution.
+
+set.seed(100) #ensure dataset is stable by using seed
+d <- rpois(25,8) #poisson dist 8 deg free
+d   #if i run this code ill get the same dataset
+
+GetCI <- function(x, level=0.95){
+  if(level <= 0 || level >=1 ){
+    #stop!
+    stop("the 'level' argument must be >0 and <1")
+  }
+  if (level< 0.5){
+    warning("Confidence levels are often close to 1, e.g. 0.95")
+  }
+  m <- mean(x)
+  n<- length(x)
+  SE <- sd(x)/sqrt(n)
+  upper <- 1-(1-level)/2
+  ci <- m + c(-1,1)*qt(upper, n-1)*SE
+  return (list(mean=m,se=SE,ci=ci))
+}
+
+GetCI(d,0.05)  #it would be nice to report the error from function
+  
+------------------------------------------------
+  #R 4.4 - Passing Additional Arguments Using an Ellipsis
+  #Sometimes it is useful to be able to pass any extra arguments to another function. For example, if a new plotting function is created that makes use of the function called plot(), it would be useful to be able to be able to specify additional details to plot() automatically. This is possible using an ellipsis, "...".
+  rm(list=ls())
+  setwd("/Adl Box/My Box Files/mooc CompDataA.R/week 1")
+  revenue <- read.csv("revenue.csv", header=FALSE)
+  head(revenue,3)
+  
+RevSummary <- function(x, ... ){
+  if(!is.matrix(x) && !is.data.frame(x)){
+    stop("'x' must be a matrix or  data frame")
+  }
+  elipsis.args <- list(...)
+  rev.per.company <- colMeans(x, ...)
+  rev.per.day     <- rowMeans(x, ...)
+  return (list( rev.per.company = rev.per.company,
+           rev.per.day = rev.per.day))
+}
+
+RevSummary(revenue)
+RevSummary(revenue, na.rm=TRUE)
+which(is.na(revenue), arr.ind=TRUE)
+
+------------------------------------------------
+  #R 4.5 - Make a Returned Result Invisible and Build Recursive Function
+  #Use the invisible() function in place of return() in a function to prevent the output from printing to the user's screen but still allow the result to be stored into an R object. Additionally, when building a recursive function, use the Recall() function.
+annoying <- function(x){
+  #return(rep(x,1000))
+  invisible(rep(x,1000)) 
+}
+
+tmp <- annoying(1:3)
+tmp
+
+#clear console 
+cmd -alt-L mac
+ctrl -L win
+
+RECURSIVE FUNCTIONS
+
+logMe <- function(x){
+  
+  return(ifelse(x>1, logMe(log(x)) , x))
+}
+logMe(3.392)
+logMe(exp(3.392))
+
+------------------------------------------------
+  #R 4.6 - Custom Functions With apply()
+  #In the last section of videos, the apply() function was introduced. A prebuilt function is often used in this function, but it is also common to write a new function within apply() or to apply a custom function.
+  
+x <- matrix(rpois(1e4,8), 1e3) 
+apply(x,2,mean)
+apply(x,2, function(x) sd(x)/sqrt(length(x)))
+or
+apply(x,2, function(x){ sd(x)/
+                          sqrt(length(x))})
+tmp <- apply(x,2,GetCI) #since its a full list
+tmp
+is.list(tmp)   #convert it to a supper list to handle it
+tmp[[1]]
+.
+  
+------------------------------------------------
+  Data sets to practice your skills
+Chickweight - experimental comparing chicken feed
+Insectsprays - experiment comparing insect srpays
+rock - rock samples frfrom petroleum reservoir
+morley - speed of light measurements from the 1879
+iris - measurements of 150 flowers of diferent types
+toothGrowth - data concerning vitamin C and teeth
+CO2 - CO2 uptake for plants under different conditions
+
+?ChickWeight
+cw <- ChickWeight
+
+------------------------------------------------
+  #source file location http://rfunction.com/code/data/stocks.txt
+------------------------------------------------
+#Calculating Memory Requirements
+  I have a data frame with 1,500,000 rows and 120 columns, all of which are numeric data. Roughly, how much memory is required to store this data frame?
+
+1,500,000 × 120 × 8 bytes/numeric
+= 1440000000 bytes
+= 1440000000 / 220 bytes/MB
+= 1,373.29 MB
+= 1.34 GB
+
+#select 100 elements at random from these 2000 values
+myData <- sample(c(y, z), 100)
+
+join the two vectors, not separated by a single space
+paste(1:3 , c("X","Y","Z"), sep="")
+paste(LETTERS, 1:4, sep="-")
+my_char <- c("My", "name", "is")
+paste( my_char, collapse= " ")
+
+x <- c(1:20)
+#R accepts negative integer indexes. 
+x[c(2, 10)] #gives us ONLY the 2nd and 10th  elements of x, 
+x[c(-2, -10)] #gives us all elements of x EXCEPT for the 2nd and 10 elements.
+x[-c(2,10)]
 ------------------------------------------------
 
 #Excercises 
